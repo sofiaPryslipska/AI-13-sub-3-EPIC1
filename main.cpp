5ef2f90bb6738da2a3e77ae73534d93e7a2ce3d9
sofiaPryslipska <146474575+sofiaPryslipska@users.noreply.github.com>
2023-10-14 14:23:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,8 @@ using namespace std;
 int main(){
     cout<< "Sofia Pruslipska\n";
     cout<< "Yuri Harhai\n";
-
-    return 0;
-}
-=======
-int main(){
-
-
-
     cout<<"Nazar Khotiienko\n";
     cout<<"Taras Kibysh\n"
-
     cout<<"Viktor Bulyshyn\n";
     
 
