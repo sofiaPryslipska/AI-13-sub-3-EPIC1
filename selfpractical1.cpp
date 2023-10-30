7459b1a13f836399f1070c6ec5ee880482cec3bd
nazar_khotiienko <146390917+QQQQQQQQQQQQQQ22@users.noreply.github.com>
2023-10-30 06:50:44 -0700

    Create selfpractical1.cpp

---
+++ selfpractical1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+int main()
+{
+  using namespace std;
+
+  double a = 0, b = 0;
+  cout << "a = ";
+  cin >> a;
+  cout << "b = ";
+  cin >> b;
+
+  cout.precision(2); // кількість знаків після коми
+  cout << "a to b power  = " << pow(a, b) << endl;
+  return 0;
+}.
